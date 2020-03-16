f8cf2fa9d32ed79e2462024930c3776b5c7fc351
anichur25 <aniscseru6125@gmail.com>
2020-03-16 19:32:22 +0600

    ok

--- E-shopper/resources/views/pages/home_content.blade.php
+++ E-shopper/resources/views/pages/home_content.blade.php
@@ -10,8 +10,8 @@
             <div class="productinfo text-center">
                 <img src="{{URL :: to($product -> product_image)}}" width = "200px" height = "280px">
                 <h2>{{ $product -> product_price }}Tk.</h2>
-                <h5>{{ $product -> product_name }}</h5>
-                <h4>{{ $product -> shop_name  }}</h4>
+                <h5 style = "color : black"><strong>{{ $product -> product_name }}</strong></h5>
+                <h4><strong style = "color : green;">Shop : </strong>{{ $product -> shop_name  }}</h4>
                 <a href="{{ URL :: to('/view-product/'. $product -> product_id) }}" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
             </div>
             <div class="product-overlay">
